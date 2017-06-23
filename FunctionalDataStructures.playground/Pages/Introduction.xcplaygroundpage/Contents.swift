8d74bd3472813d4581efbdf4fe167cafce94efe2
Andrey Denisov <andreyd@hexagontech.co.il>
2017-06-23 13:02:15 +0300

    Initial commit : add playground

---
+++ FunctionalDataStructures.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ### Introduction
+
+This is playground, which illustrate different data structures from Okasaki book
+ */
+
+//: [Next](@next)
