c48a085e5827c37d8c267370c5efb45bbaa50021
Andrey Denisov <andreyd@hexagontech.co.il>
2017-06-23 17:48:41 +0300

    LeftistHeap : add base methods

--- FunctionalDataStructures.playground/Pages/LeftistHeap.xcplaygroundpage/Contents.swift
+++ FunctionalDataStructures.playground/Pages/LeftistHeap.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 
 indirect enum LeftistHeap <Element:Comparable> {
     case leaf
-    case node(LeftistHeap<Element>, Element, LeftistHeap<Element>)
+    case node(Int, Element, LeftistHeap<Element>, LeftistHeap<Element>)
 }
 
 extension LeftistHeap {
@@ -17,10 +17,20 @@ extension LeftistHeap {
 
 extension LeftistHeap {
     
+    func getRank() -> Int {
+        switch self {
+        case .leaf: return 0
+        case .node(let rank,_ , _ , _) :  return rank
+        }
+    }
+}
+
+extension LeftistHeap {
+    
     var isEmpty : Bool {
         switch self {
         case .leaf : return true
-        case .node(_, _, _) : return false
+        case .node(_,_, _, _) : return false
         }
     }
 
@@ -29,5 +39,42 @@ extension LeftistHeap {
     }
 }
 
+extension LeftistHeap {
+    
+    func insert(object:Element) -> LeftistHeap {
+        return self
+    }
+}
+
+extension LeftistHeap {
+    
+    func merge(with heap:LeftistHeap) -> LeftistHeap {
+        return heap
+    }
+    
+    func makeT() -> LeftistHeap {
+        return self
+    }
+}
+
+extension LeftistHeap {
+    
+    func findMin() -> Element? {
+        switch self {
+        case .leaf: return nil
+        case .node(_, let min, _, _ ) : return min
+        }
+    }
+    
+    func deleteMin() -> LeftistHeap? {
+        switch self {
+        case .leaf: return nil
+        case .node(_, _ , _, _ ) : return self
+        }
+    }
+}
+
+
+let heap = LeftistHeap<Int>()
 
 //: [Next](@next)
