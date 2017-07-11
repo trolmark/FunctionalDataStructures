fbe8dfcd45788bf74af022fb8398455cde8884d0
Andrey Denisov <andreyd@hexagontech.co.il>
2017-07-11 11:39:31 +0300

    BatchedQueue : add handling invariant

--- FunctionalDataStructures.playground/Pages/Queue.xcplaygroundpage/Contents.swift
+++ FunctionalDataStructures.playground/Pages/Queue.xcplaygroundpage/Contents.swift
@@ -14,8 +14,8 @@ protocol Queue {
 struct BatchedQueue<Elem> : Queue {
     typealias Element = Elem
     
-    fileprivate var f : [Elem]
-    fileprivate var r : [Elem]
+    fileprivate var f : [Elem] = []
+    fileprivate var r : [Elem] = []
     
     func isEmpty() -> Bool {
         return f.isEmpty
@@ -26,15 +26,35 @@ struct BatchedQueue<Elem> : Queue {
     }
     
     func tail() -> BatchedQueue<Elem> {
-        let newR = Array(r.dropFirst())
-        return BatchedQueue(f: f, r: newR)
+        let newF = Array(f.dropFirst())
+        return checkF(f: newF, r: r)
     }
     
     func snoc(elem: Elem) -> BatchedQueue<Elem> {
-        let newR = r + [elem]
-        return BatchedQueue(f: f, r: newR)
+        let newR =  [elem] + r
+        return checkF(f: f, r: newR)
+    }
+    
+    /* Elements are added to r and removed from f , so they must somehow migrate from 
+     one list to the other. This is accomplished by reversing R and installing the result 
+     as the new F whenever F would otherwise become empty, simultaneously setting the new R to [ ] */
+    
+    func checkF(f:[Elem], r:[Elem]) -> BatchedQueue<Elem> {
+        if f.isEmpty {
+            return BatchedQueue(f: r.reversed(), r: [])
+        } else {
+            return BatchedQueue(f: f, r: r)
+        }
     }
 }
+
+let queue = BatchedQueue<Int>()
+print(queue
+    .snoc(elem: 1)
+    .snoc(elem: 2)
+    .snoc(elem: 7)
+    .tail()
+    .head())
 
 
 //: [Next](@next)
