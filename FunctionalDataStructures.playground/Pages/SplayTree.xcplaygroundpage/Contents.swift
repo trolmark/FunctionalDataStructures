17fe22223580c2b51736e499b12e4b800348b6f7
Andrey Denisov <andreyd@hexagontech.co.il>
2017-07-12 17:41:10 +0300

    SplayTree: add insert, delete and findMin implementation

--- FunctionalDataStructures.playground/Pages/SplayTree.xcplaygroundpage/Contents.swift
+++ FunctionalDataStructures.playground/Pages/SplayTree.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 
 enum SplayTree<Elem:Comparable> {
     case empty
-    indirect case node(SplayTree, Elem, SplayTree)
+    indirect case node(SplayTree<Elem>, Elem, SplayTree<Elem>)
 }
 
 
@@ -56,19 +56,42 @@ extension SplayTree : Heap {
     typealias Element = Elem
     
     func merge(with heap:SplayTree<Elem>) -> SplayTree<Elem> {
-        return heap
+        switch self {
+        case .empty:
+            return heap
+        case let .node(left, value, right) :
+            let (ta, tb) = partition(tree: heap, with: value)
+            return .node(ta.merge(with: left), value, tb.merge(with: right))
+        }
     }
     
     func insert(x: Elem) -> SplayTree<Elem> {
-        return self
+        let (a,b) = partition(tree: self, with: x)
+        return .node(a, x, b)
     }
     
-    func deleteMin() -> SplayTree<Elem>? {
-        return self
+    func deleteMin() -> SplayTree<Elem> {
+        
+        switch self {
+        case .empty:
+            return .empty
+        case let .node(.empty, _, right) :
+            return right
+        case let .node(.node(a, x, b), value, right):
+            return .node(a.deleteMin(), x, .node(b, value, right))
+        
+        }
     }
     
     func findMin() -> Elem? {
-        return nil
+        switch self {
+        case .empty:
+            return nil
+        case let .node(.empty, value, _) :
+            return value
+        case let .node(left, _, _) :
+            return left.findMin()
+        }
     }
 }
 
--- FunctionalDataStructures.playground/Sources/Heap.swift
+++ FunctionalDataStructures.playground/Sources/Heap.swift
@@ -5,7 +5,7 @@ public protocol Heap {
     associatedtype Element
     
     func findMin() -> Element?
-    func deleteMin() -> Self?
+    func deleteMin() -> Self
     func merge(with heap:Self) -> Self
     func insert(x:Element) -> Self
 }
