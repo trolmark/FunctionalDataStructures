1906cbe92de11aa6748df056f3ea7bc10d7ca9ad
Andrey Denisov <andreyd@hexagontech.co.il>
2017-07-12 18:08:32 +0300

    SplayTree : add partition method

--- FunctionalDataStructures.playground/Pages/SplayTree.xcplaygroundpage/Contents.swift
+++ FunctionalDataStructures.playground/Pages/SplayTree.xcplaygroundpage/Contents.swift
@@ -17,6 +17,11 @@ extension SplayTree: CustomStringConvertible {
             return root + "\n"
         case let .node(.empty,value, .empty):
             return root + "\(value)\n"
+            
+        case let .node(left, value, .empty):
+            return root + "\(value)\n"
+                + left.diagram(bottom + "│   ", bottom + "└───", bottom + "    ")
+        
         case let .node(left, value, right):
             return right.diagram(top + "    ", top + "┌───", top + "│   ")
                 + root + "\(value)\n"
@@ -47,7 +52,39 @@ extension SplayTree {
 private extension SplayTree {
     
     func partition(tree:SplayTree, with pivot:Elem) -> (SplayTree, SplayTree) {
-        return (tree, tree)
+        switch tree {
+        case .empty:
+            return (.empty, .empty)
+            
+        case let .node(left, value, right):
+            
+            if value < pivot {
+                switch right {
+                case .empty: return (tree, .empty)
+                case let .node(left1, value1, right1):
+                    if value1 < pivot {
+                        let (small, big) = partition(tree: right1, with: pivot)
+                        return (.node(.node(left, value, left1), value1, small), big)
+                    } else {
+                        let (small, big) = partition(tree: left1, with: pivot)
+                        return (.node(left, value, small), .node(big, value1, right1))
+                    }
+                }
+                
+            } else {
+                switch left {
+                case .empty: return (.empty, tree)
+                case let .node(left1, value1, right1):
+                    if value1 < pivot {
+                        let (small, big) = partition(tree: right1, with: pivot)
+                        return (.node(left1, value1, small), .node(big, value, right))
+                    } else {
+                        let (small, big) = partition(tree: left1, with: pivot)
+                        return (small, .node(big, value1, .node(right1, value, right)))
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -94,5 +131,24 @@ extension SplayTree : Heap {
         }
     }
 }
+
+extension SplayTree {
+    
+    init(from list:[Elem]) {
+        
+        if list.isEmpty {
+            self = .empty
+        } else {
+            self = list.reduce(.empty, { result, item in
+                return result.insert(x: item)
+            })
+        }
+    }
+}
+
+let list = [11, 1, 2, 3, 4, 5 ]
+var splayHeap = SplayTree<Int>.init(from:list)
+print(splayHeap)
+
 
 //: [Next](@next)
