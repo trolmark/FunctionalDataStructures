2a50dffec0e414acf4dcc8c0ed43442eb406bd16
Andrey Denisov <andreyd@hexagontech.co.il>
2017-07-20 11:09:01 +0300

    CatenableList : add basic method implementation + printing structure

--- FunctionalDataStructures.playground/Pages/CatenableList.xcplaygroundpage/Contents.swift
+++ FunctionalDataStructures.playground/Pages/CatenableList.xcplaygroundpage/Contents.swift
@@ -2,11 +2,16 @@
 
 import Foundation
 
-struct BatchedQueue<Elem> : Queue {
+struct BatchedQueue<Elem:CustomStringConvertible> : Queue {
+    
     typealias Element = Elem
     
     fileprivate var f : [Elem] = []
     fileprivate var r : [Elem] = []
+    
+    static func empty() -> BatchedQueue {
+        return BatchedQueue(f: [], r: [])
+    }
     
     func isEmpty() -> Bool {
         return f.isEmpty
@@ -17,8 +22,12 @@ struct BatchedQueue<Elem> : Queue {
     }
     
     func tail() -> BatchedQueue<Elem> {
-        let newF = Array(f.dropFirst())
-        return checkF(f: newF, r: r)
+        if f.isEmpty {
+            return checkF(f: [], r: r)
+        } else {
+            let newF = Array(f.dropFirst())
+            return checkF(f: newF, r: r)
+        }
     }
     
     func snoc(elem: Elem) -> BatchedQueue<Elem> {
@@ -35,11 +44,44 @@ struct BatchedQueue<Elem> : Queue {
     }
 }
 
+extension BatchedQueue: CustomStringConvertible {
+    
+    var description: String {
+        let all = f + r
+        return all.reduce("", { result, elem in
+            return result + "   " + elem.description
+        })
+    }
+}
 
 
 indirect enum CatenableList<Element:Comparable> {
     case empty
     case node(Element, BatchedQueue<CatenableList>)
+}
+
+extension CatenableList {
+    
+    init() {
+        self = .empty
+    }
+}
+
+extension CatenableList: CustomStringConvertible {
+    
+    func diagram(_ root: String, _ bottom: String) -> String {
+        switch self {
+        case .empty:
+            return root + "\n"
+        case let .node(value, queue):
+            return root + "\(value)\n"
+                + queue.description
+        }
+    }
+    
+    var description: String {
+        return self.diagram("", "")
+    }
 }
 
 
@@ -62,23 +104,31 @@ extension CatenableList {
         }
     }
     
-    
     func snoc(elem: Element) -> CatenableList {
-        return self
+        return link(elem: .node(elem, BatchedQueue.empty()))
     }
         
         
     func link(elem: CatenableList) -> CatenableList {
         switch self {
         case .empty:
-            return .empty
+            return elem
         case let .node(x, q):
             return .node(x, q.snoc(elem: elem))
         }
     }
     
     func linkAll(in queue:BatchedQueue<CatenableList>) -> CatenableList {
-        return self
+        
+        guard let qHead = queue.head()
+        else { return .empty }
+            
+        let qTail = queue.tail()
+        
+        if qTail.isEmpty() {
+            return qHead
+        }
+        return qHead.linkAll(in: qTail)
     }
     
     func isEmpty() -> Bool {
@@ -90,5 +140,13 @@ extension CatenableList {
         }
     }
 }
+
+let list = CatenableList<Int>()
+print(list.snoc(elem: 6)
+    .snoc(elem: 4)
+    .snoc(elem: 3)
+    .head())
+
+
 
 //: [Next](@next)
